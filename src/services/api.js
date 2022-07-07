ef06bbc6c1d11ee5ea6d009ac36da3731de1b9a7
Lucas Moura <93lucasribeiro@gmail.com>
2022-07-07 16:36:46 -0300

    ex1 com identação corrigida

--- src/services/api.js
+++ src/services/api.js
@@ -4,24 +4,18 @@ export async function getCategories() {
   return result;
 }
 
-export async function getProductsFromCategoryAndQuery( categoryId, query ) {
-
-  const queryUrl= 'https://api.mercadolibre.com/sites/MLB/search?'
-
-  let url= '';
-
-if (!categoryId) {
-  url = `${queryUrl}category=${query}`
-
-} else if (!query) {
-  url = `${queryUrl}category=${categoryId}`
-} else if (categoryId && query) {
-  url = `${queryUrl}category=${categoryId}&q=$${query}`
-}
-
-const response = await fetch(url)
-const result = await response.json()
-
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const queryUrl = 'https://api.mercadolibre.com/sites/MLB/search?';
+  let url = '';
+  if (!categoryId) {
+    url = `${queryUrl}category=${query}`;
+  } else if (!query) {
+    url = `${queryUrl}category=${categoryId}`;
+  } else if (categoryId && query) {
+    url = `${queryUrl}category=${categoryId}&q=$${query}`;
+  }
+  const response = await fetch(url);
+  const result = await response.json();
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   return result;
 }
