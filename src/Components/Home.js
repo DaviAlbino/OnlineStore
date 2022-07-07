cef65a465c5ce3f7bd848c5dc04df3a7d841fdd4
Rafael de figueiredo Lopes <rafaelflopes18@gmail.com>
2022-07-07 20:10:57 -0300

    grupo-4-requisito-4

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  state = {
+    allCategories: '',
+  }
+
+  componentDidMount() {
+    this.getListCategories();
+  }
+
+  getListCategories= async () => {
+    const categories = await getCategories();
+    this.setState({ allCategories: categories });
+    console.log(categories);
+  }
+
   render() {
+    const { allCategories } = this.state;
     return (
       <div>
+        {allCategories && allCategories.map((category) => (
+          <label key={ category.id } htmlFor={ category.id } data-testid="category">
+            {category.name}
+            <input
+              type="radio"
+              name="category"
+              id={ category.id }
+            />
+          </label>
+        ))}
         <div>
           <input
             type="text"
