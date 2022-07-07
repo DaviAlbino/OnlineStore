c3d52789930e70f5f623e088da5222a1c5272af7
Lucas Moura <93lucasribeiro@gmail.com>
2022-07-07 20:34:27 -0300

    ex5 incompleto

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   state = {
     allCategories: '',
+    searchInput: '',
   }
 
   componentDidMount() {
     this.getListCategories();
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox'
+      ? target.checked : target.value; this.setState({ [name]: value });
+  }
+
+  getSearchApi = async () => {
+    const { searchInput } = this.state;
+    // const data = await getProductsFromCategoryAndQuery(searchInput);
   }
 
   getListCategories= async () => {
@@ -18,7 +30,7 @@ class Home extends React.Component {
   }
 
   render() {
-    const { allCategories } = this.state;
+    const { allCategories, searchInput } = this.state;
     return (
       <div>
         {allCategories && allCategories.map((category) => (
@@ -32,13 +44,17 @@ class Home extends React.Component {
           </label>
         ))}
         <div>
-          <input
+        <input
             type="text"
-            name="search-imput"
+            name="searchInput"
             placeholder=""
+            data-testid="query-input"
+            value={ searchInput }
+            onChange={ this.handleChange }
           />
         </div>
         <div data-testid="home-initial-message">
+        <button type="button" data-testid="query-button">Pesquisar</button>
           <h4>
             Digite algum termo de pesquisa ou escolha uma categoria.
           </h4>
