58dbdd60cbd0966027dc0f5a66b59f24e4d2ea02
Lucas Moura <93lucasribeiro@gmail.com>
2022-07-08 13:00:46 -0300

    lint ex5 incompleto

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   let url = '';
-  const queryUrl = 'https://api.mercadolibre.com/sites/MLB/search?'
+  const queryUrl = 'https://api.mercadolibre.com/sites/MLB/search?';
 
   if (categoryId && query) {
     url = `${queryUrl}category=${categoryId}&q=${query}`;
@@ -14,8 +14,8 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
     url = `${queryUrl}category=${categoryId}`;
   } else if (!categoryId) {
     url = `${queryUrl}q=${query}`;
-  } 
-  
+  }
+
   const response = await fetch(url);
   const result = await response.json();
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
